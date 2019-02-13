78f6d189527cfccf3be9a378408351b5b58420b0
wan03 <38962452+wan03@users.noreply.github.com>
2019-02-13 14:40:20 -0500

    Added Web.config and everything works

--- resources/js/components/Pages/HomePage/HomeHeader.js
+++ resources/js/components/Pages/HomePage/HomeHeader.js
@@ -5,17 +5,17 @@ import { UncontrolledCarousel } from 'reactstrap';
 
 const items = [
   {
-    src: 'http://deershire.com/wp-content/uploads/2019/slide-01.jpg',
+    src: 'https://deershire.com/wp-content/uploads/2019/slide-01.jpg',
     altText: 'Una muchacha de pelo rizo castaño bregando con sú celular arriba de un edificio. El Titulo dice: Para que esperar? Desbloquea tu celular ahora.',
 
   },
   {
-    src: 'http://deershire.com/wp-content/uploads/2019/slide-02.jpg',
+    src: 'https://deershire.com/wp-content/uploads/2019/slide-02.jpg',
     altText: 'Un muchaho sonriendo debloqueando sú celular. El Titulo dice: Rapido Y Seguro, Liberate de servicios no queridos.',
 
   },
   {
-    src: 'http://deershire.com/wp-content/uploads/2019/slide-03.jpg',
+    src: 'https://deershire.com/wp-content/uploads/2019/slide-03.jpg',
     altText: 'Teléfonos antiguos de rueda en diferentes colores. El Titulo dice: Adios celular antiguo, Desbloquealo y vendelo.',
 
   }
