a4a75345dc368f1670d9c52c50934e32079d85fb
wan03 <38962452+wan03@users.noreply.github.com>
2019-02-13 18:50:01 -0500

    ran npm prod

--- resources/js/components/Pages/SearchProductPage/PhoneResults/PhoneResultsWrapper.js
+++ resources/js/components/Pages/SearchProductPage/PhoneResults/PhoneResultsWrapper.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Phone from './../../../Phone/Phone';
+import Phone from "./../../../Phone/Phone";
 import {
-    Card, Button, CardImg, CardTitle, CardText, CardDeck,
-    CardSubtitle, CardBody, CardColumns
-} from 'reactstrap';
-
+    Card,
+    Button,
+    CardImg,
+    CardTitle,
+    CardText,
+    CardDeck,
+    CardSubtitle,
+    CardBody,
+    CardColumns
+} from "reactstrap";
 
 class PhoneResultsWrapper extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -17,46 +22,42 @@ class PhoneResultsWrapper extends React.Component {
         };
     }
     componentDidMount() {
-        axios.get('/api/mobiles').then(response => {
-            this.setState({
-
-                phones: response.data.data
+        axios
+            .get("/api/mobiles")
+            .then(response => {
+                this.setState({
+                    phones: response.data.data
+                });
+                console.log("THIS IS RESPONSE", response.data.data);
+            })
+            .catch(errors => {
+                // console.log(errors);
             });
-            console.log('THIS IS RESPONSE', response.data.data)
-
-        }).catch(errors => {
-            // console.log(errors);
-        })
     }
-
 
     render() {
         return (
-            <div >
+            <div>
                 <CardColumns>
                     <CardDeck />
                     {this.state.phones.map((phone, i) => {
                         if (i < 100) {
                             return (
                                 <Phone
-
                                     img={phone.id}
                                     name={phone.mobiles[0].mobileName}
                                     img={phone.mobiles[0].mobilePhoto}
                                     key={phone.id}
                                     phoneId={phone.id}
                                 />
-                            )
+                            );
                         }
-                    })
-                    }
+                    })}
                     <CardDeck />
                 </CardColumns>
             </div>
         );
-    };
+    }
 }
 
 export default PhoneResultsWrapper;
-
-
